cb88ad6395e173db25cf4ffa00f7c5f2c9871e1f
feifeibear <fangjiarui123@gmail.com>
2018-09-03 20:16:03 +0800

    indent better

--- swCNNv10-4cg-image-size-aware/gemm.c
+++ swCNNv10-4cg-image-size-aware/gemm.c
@@ -24,14 +24,14 @@ void gemm(Type* input, Type* weight, Type* output, int M, int N, int K, int rid,
 
   for(ccCore=0; ccCore<8; ++ccCore){
     for(cM = 0; cM < M; cM += 4){
-		  Type* output_ptr = output + 4*cM;
+      Type* output_ptr = output + 4*cM;
       for(cN = 0; cN < N; cN += 4){
-			  for(i = 0 ; i < 4; ++i)
-			    for(j = 0; j < 4; ++j)
-			      simd_load( tmp_output[i*4+j],(output_ptr+4*(j+i*M)) );
+        for(i = 0 ; i < 4; ++i)
+          for(j = 0; j < 4; ++j)
+            simd_load( tmp_output[i*4+j],(output_ptr+4*(j+i*M)) );
 
-			  Type* weight_ptr = weight + cN;
-			  Type* input_ptr  = input  + 4*cM;
+        Type* weight_ptr = weight + cN;
+        Type* input_ptr  = input  + 4*cM;
         for(cK=0; cK < K; ++cK){
           if(ccCore == cid){
             simd_load(tmp_input[0], input_ptr) ;
@@ -49,14 +49,14 @@ void gemm(Type* input, Type* weight, Type* output, int M, int N, int K, int rid,
             tmp_input[3] = simd_getr(tmp_input[3]);
           }
           //FJR trans              
-			  	if(ccCore == rid){
+          if(ccCore == rid){
             simd_loade(tmp_weight[0], weight_ptr);
-	        	simd_putc(tmp_weight[0], 8);
-	         	simd_loade(tmp_weight[1], weight_ptr + 1);
-	         	simd_putc(tmp_weight[1], 8);
-	         	simd_loade(tmp_weight[2], weight_ptr + 2);
-	         	simd_putc(tmp_weight[2], 8);
-	         	simd_loade(tmp_weight[3], weight_ptr + 3);
+            simd_putc(tmp_weight[0], 8);
+            simd_loade(tmp_weight[1], weight_ptr + 1);
+            simd_putc(tmp_weight[1], 8);
+            simd_loade(tmp_weight[2], weight_ptr + 2);
+            simd_putc(tmp_weight[2], 8);
+            simd_loade(tmp_weight[3], weight_ptr + 3);
             simd_putc(tmp_weight[3], 8);
           } else{
             tmp_weight[0] = simd_getc(tmp_weight[0]);
@@ -65,23 +65,23 @@ void gemm(Type* input, Type* weight, Type* output, int M, int N, int K, int rid,
             tmp_weight[3] = simd_getc(tmp_weight[3]);
           }
 
-			    for(i = 0; i<4; ++i){
+          for(i = 0; i<4; ++i){
             for(j = 0; j<4; ++j){
               tmp_output[i*4+j] += tmp_input[j]*tmp_weight[i];
             }
           }
 
-			    weight_ptr += N;
-			    input_ptr  += 4*M;
-			  }//cK
-		    for(i = 0 ; i < 4; ++i)
+          weight_ptr += N;
+          input_ptr  += 4*M;
+        }//cK
+        for(i = 0 ; i < 4; ++i)
           for(j = 0; j < 4; ++j)
             simd_store(  tmp_output[i*4+j],(output_ptr+4*(j+i*M)) );
 
         weight_ptr += 4;
         output_ptr += 16*M;
-		  }//cN
-	  }//cM
+      }//cN
+    }//cM
   }//ccCore
 }
 
